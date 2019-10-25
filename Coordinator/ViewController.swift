6a71bbaccdcc93767fb05b1c1078d9024dcfe795
Cory Sullivan <cory@solbits.com>
2019-10-25 14:23:49 -0300

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Cory Sullivan on 2019-10-25.
+//  Copyright © 2019 Viacom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
